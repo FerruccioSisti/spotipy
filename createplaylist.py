1b5cc7f089df1317ed2318f0b7f5bb11022e5d2d
Ferruccio Sisti <ferrucciosisti@gmail.com>
2021-10-14 15:12:30 -0500

    removed hardcoded API tokens

--- createplaylist.py
+++ createplaylist.py
@@ -2,11 +2,8 @@ import os
 from spotifyclient import SpotifyClient
 
 def main():
-    # tok = "BQB5hH9N98VIs4p6FHLO7sxZikUCt-67c0ZUn1jcU3i3-A6any5w1vaMmfK1TczZvEtd7x7NfC0RpLtAUgAfP5r1B7UAX_LV78oOy7vgOC5tsTjTrlI6Yi0E0YtPznHGQKA44860eQV27m2F-awzw3zPvNCPBjQcdEBVN9LlIMR4W9SgPPdLRoD81l2E7cUvAJh4YvEfJWUj4oaoV_EuepJ-xW4qUR9or6TrC__3eExSMjh3"
-    # usrid = "BQBrhf5CKe8OoWsiMg-ISO-3jvyaTaVUvmuUz5ch28y5p4ajtbex5T3Sv4Rnxzq0k-ezYnBegneyw8Ygp-fImAsecuhXliDMLN7xMyIkKBVpaA1ENcofr5FyKP_ass9gsiqGkxFk978eJCXHuM_CPM6G4AntE8MOBU6PtFe0TpL9V7v-V8RdgeUZHFNHxNiG30eB-inMrKQ9YMqJLWtE0xbP7WVfmPddVQwlPgXEXsi1wsCl"
     #instantiate spotify client
     spotify_client = SpotifyClient(os.getenv("SPOTIFY_AUTHORIZATION_TOKEN"), os.getenv("SPOTIFY_USER_ID"))
-    # spotify_client = SpotifyClient(tok, usrid)
 
     #get last played tracks
     num_tracks_to_visualize = int((input("How many tracks would you like to visualize/take data from?")))
